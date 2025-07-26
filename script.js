d4a56f2af9d36aaff1be30a7c44e8e8db75340d0
Declan Williams <declanwilliams793@gmail.com>
2025-07-26 14:58:18 +0100

    Create episode container and basic DOM structure

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.innerHTML = ""; // clear previous contents so dont keep adding to it when function is called multiple times
 
+    // create a div container element to store episodes
+    episodeContainer = document.createElement("div");
+    episodeContainer.className = "episode-container";
+
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
