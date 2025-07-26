b8c575bcfd7f3f0f8f83524031f1150150e1f3a9
Declan Williams <declanwilliams793@gmail.com>
2025-07-26 13:54:34 +0100

    implemented clear root element before rendering to prevent duplicates

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  rootElem.innerHTML = ""; // clear previous contents so dont keep adding to it when function is called multiple times
+
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
