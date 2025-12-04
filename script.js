bc28ae52bcff78501639f1b0d219ce5d59b8af46
Declan Williams <declanwilliams793@gmail.com>
2025-12-04 01:38:33 +0000

    refactor: reorganize makePageForEpisodes function and add makeEpisodeCode utility

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
+// create a function to create and padStart episode code (S01 E10)
+function makeEpisodeCode(season, episode) {
+  const seasonStr = season.toString().padStart(2, "0");
+  const episodeStr = episode.toString().padStart(2, "0");
+  return `S${seasonStr}E${episodeStr}`;
+}
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.innerHTML = ""; // clear previous contents so dont keep adding to it when function is called multiple times
 
-    // create a div container element to store episodes
-    const episodeContainer = document.createElement("div");
-    episodeContainer.className = "episode-container";
+  // create a div container element to store episodes
+  const episodeContainer = document.createElement("div");
+  episodeContainer.className = "episode-container";
 
-    // loop through each episode in episodeList
-    for (const episode of episodeList){
-      // create a div for each episode
-      const episodeElem = document.createElement("div");
-      episodeElem.className = "episode"; 
-
-      // append episode div to the container
-      episodeContainer.appendChild(episodeElem);
-    }
-// append the container with episodes to the rootElem to display on the page
-rootElem.appendChild(episodeContainer);
+  // loop through each episode in episodeList
+  for (const episode of episodeList) {
+    // create a div for each episode
+    const episodeElem = document.createElement("div");
+    episodeElem.className = "episode";
+    // append episode div to the container
+    episodeContainer.appendChild(episodeElem);
+  }
+  // append the container with episodes to the rootElem to display on the page
+  rootElem.appendChild(episodeContainer);
 }
 
 window.onload = setup;
